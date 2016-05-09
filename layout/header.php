d15b8a2b7052ffaa4ec5ebedd96d078aff2616ad
Nkelliny <sitelegion@coolsite.net>
2016-05-08 21:14:56 -0400

    Added files via upload

---
+++ layout/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php if(isset($title)){ echo $title; }?></title>
+    <link rel="stylesheet" href="layout/bootstrap.css">
+    <link rel="stylesheet" href="layout/extra.css">
+    <link rel="stylesheet" href="style/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="icon" type="image/png" href="layout/favicon.png">
+</head>
+<body>
